0a991ccfd56bc9bc592479f82dc6e188f10c3203
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2022-07-28 09:15:55 -0700

    beyblade, full rip

--- 01-scraping/index.js
+++ 01-scraping/index.js
@@ -1,20 +1,21 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-let idList = ["aliipoe","aloalo","amau","ape","auhuhu","awa","awapuhi","awikiwiki","hala","hala_pepe","hame","hao","hapuu","hau","hinahina","hoawa","hoi","holei","ieie","iliahi","iliee","ilima","ipu","kalia","kalo","kamani","kauila_A","kauila_C","kaunaoa","kawau","kawelu","ki","ki_nehe","ko","koa","koaia","koali_ai","koali_awa","kohekohe","kokio","kokio_keokeo","kolea","kolokolo_uahiwi","kookoolau","kopiko","kou","kukaenene","kukaepuaa","kukui","kupukupu","laamia","lama","lauae","laukahi_kuahiwi","lehua_papa","loulu","maaloa","maia","maia_polapola","maile","makaloa","mamaki","mamane","manono","mao_A","mao_G","maua","mauu_laili","mehamehame","milo","moa","mokihana","naenae","naio","nanu","naupaka_kahakai","naupaka_kuahiwi","nehe","nioi","niu","noni","nuku_iiwi","ohai","oha_wai","ohe","ohelo","ohe_makai","ohe_ohe","ohia_ai","ohia_ha","ohia_lehua","okaha","olapa","olena","olomea","olona","olopua","opuhe","paihi","painu","pala","palaa","palapalai","pamoho","papala","papala_kepau","pia_D","pia_T","pili","pilo","pohinahina","pohuehue","popolo","popolokumai","pua_kala","pukiawe","uala","uhaloa","uhi","uhiuhi","uki","ukiuki","ulei","ulu","uluhe","wauke","wiliwili"];
 
 (async () => {
 
-  const jsonFile = "./ethnobotany.json";
-  const currentFlower = idList[4];
-  const theURL = 'http://data.bishopmuseum.org/ethnobotanydb/ethnobotany.php?b=d&ID=' + currentFlower;
+  let pageKeys = ["aliipoe","aloalo","amau","ape","auhuhu","awa","awapuhi","awikiwiki","hala","hala_pepe","hame","hao","hapuu","hau","hinahina","hoawa","hoi","holei","ieie","iliahi","iliee","ilima","ipu","kalia","kalo","kamani","kauila_A","kauila_C","kaunaoa","kawau","kawelu","ki","ki_nehe","ko","koa","koaia","koali_ai","koali_awa","kohekohe","kokio","kokio_keokeo","kolea","kolokolo_uahiwi","kookoolau","kopiko","kou","kukaenene","kukaepuaa","kukui","kupukupu","laamia","lama","lauae","laukahi_kuahiwi","lehua_papa","loulu","maaloa","maia","maia_polapola","maile","makaloa","mamaki","mamane","manono","mao_A","mao_G","maua","mauu_laili","mehamehame","milo","moa","mokihana","naenae","naio","nanu","naupaka_kahakai","naupaka_kuahiwi","nehe","nioi","niu","noni","nuku_iiwi","ohai","oha_wai","ohe","ohelo","ohe_makai","ohe_ohe","ohia_ai","ohia_ha","ohia_lehua","okaha","olapa","olena","olomea","olona","olopua","opuhe","paihi","painu","pala","palaa","palapalai","pamoho","papala","papala_kepau","pia_D","pia_T","pili","pilo","pohinahina","pohuehue","popolo","popolokumai","pua_kala","pukiawe","uala","uhaloa","uhi","uhiuhi","uki","ukiuki","ulei","ulu","uluhe","wauke","wiliwili"];
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  //network idle tells it to wait until all things have loaded on the page
-  await page.goto(theURL, {waitUntil: 'networkidle2'});
-  
-  //.evaluate is like running a console command
+  for (let i = 0; i < pageKeys.length; i++) {
+    const theURL = 'http://data.bishopmuseum.org/ethnobotanydb/ethnobotany.php?b=d&ID=' + pageKeys[i];
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    //network idle tells it to wait until all things have loaded on the page
+    await page.goto(theURL, {waitUntil: 'networkidle2'});
+
+    const jsonFile = "./ethnobotany.json";
+
+      //.evaluate is like running a console command
   let data = await page.evaluate(()=> {
 
     //remove all br tags
@@ -55,19 +56,20 @@ let idList = ["aliipoe","aloalo","amau","ape","auhuhu","awa","awapuhi","awikiwik
     }
 
     return content;
-  })
+    })
   
-  //uncomment the line below when you're ready to write something
-  //fs.writeFileSync(jsonFile, JSON.stringify(data));
+    //uncomment the line below when you're ready to write something
+    //fs.writeFileSync(jsonFile, JSON.stringify(data));
 
-  fs.appendFile(jsonFile, JSON.stringify(data) + "," + "\r\n", function (err) {
-    if (err) throw err;
-    console.log('Saved to File!');
-  });
-  
-  //uncomment the line below when you need to debug
-  //console.log(data);
-  await browser.close();
+    fs.appendFile(jsonFile, JSON.stringify(data) + "," + "\r\n", function (err) {
+      if (err) throw err;
+      console.log('Saved ' + pageKeys[i] + " to file!");
+    });
+
+    //uncomment the line below when you need to debug
+    //console.log(data);
+    await browser.close();
+  }
 
   //in your terminal run 'node index.js'
 
